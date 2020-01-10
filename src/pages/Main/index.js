c6469dde05f06f21ba77087acc77adcfb828e96a
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2020-01-09 23:22:42 -0300

    Main + Reactotron + LocalStorage + Hooks

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 import { Link } from 'react-router-dom';
@@ -6,23 +6,44 @@ import { Link } from 'react-router-dom';
 import { Container } from './styles';
 
 const schema = Yup.object().shape({
-  cnpj: Yup.string().required('Esse campo é obrigatório'),
+  cnpj: Yup.string()
+    .max(10)
+    .required('Esse campo é obrigatório'),
   rs: Yup.string().required('Esse campo é obrigatório'),
   nome: Yup.string().required('Esse campo é obrigatório'),
-  cpf: Yup.number()
-    .min(11)
+  cpf: Yup.string()
+    .max(11)
     .required('Esse campo é obrigatório'),
   rua: Yup.string().required('Esse campo é obrigatório'),
   bairro: Yup.string().required('Esse campo é obrigatório'),
-  cep: Yup.number()
-    .min(8)
+  cep: Yup.string()
+    .max(8)
     .required('Esse campo é obrigatório'),
   cidade: Yup.string().required('Esse campo é obrigatório'),
   estado: Yup.string().required('Esse campo é obrigatório'),
 });
 
 export default function Main() {
-  function handleSubmit(data) {}
+  const [company, setCompany] = useState({
+    cpnj: '',
+    cpf: '',
+    cep: '',
+    rs: '',
+    nome: '',
+    rua: '',
+    bairro: '',
+    complemento: '',
+    cidade: '',
+    estado: '',
+  });
+
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  useEffect(() => {
+    localStorage.setItem('company', JSON.stringify(company));
+  }, [company]);
 
   return (
     <Container>
@@ -32,18 +53,72 @@ export default function Main() {
       />
       <h1>Cadastro de empresas</h1>
       <Form schema={schema} onSubmit={handleSubmit}>
-        <Input name="cnpj" placeholder="CNPJ" />
-        <Input name="rs" placeholder="Razão social" />
-        <Input name="nome" placeholder="Nome do responsável" />
-        <Input name="cpf" placeholder="CPF (somente números)" />
+        <Input
+          name="cnpj"
+          placeholder="CNPJ"
+          value={company.cnpj}
+          onChange={e => setCompany({ ...company, cnpj: e.target.value })}
+        />
+        <Input
+          name="rs"
+          value={company.rs}
+          placeholder="Razão social"
+          onChange={e => setCompany({ ...company, rs: e.target.value })}
+        />
+        <Input
+          name="nome"
+          vale={company.nome}
+          placeholder="Nome do responsável"
+          onChange={e => setCompany({ ...company, nome: e.target.value })}
+        />
+        <Input
+          name="cpf"
+          value={company.cpf}
+          placeholder="CPF"
+          onChange={e => setCompany({ ...company, cpf: e.target.value })}
+        />
         <h2>Endereço</h2>
-        <Input name="rua" placeholder="Rua/Avenida..." />
-        <Input name="bairro" placeholder="Bairro" />
-        <Input name="cep" placeholder="CEP (somente números)" />
-        <Input name="complemento" placeholder="Complemento" />
-        <Input name="cidade" placeholder="Cidade" />
-        <Input name="estado" placeholder="Estado" />
-        <button type="submit">Cadastrar</button>
+        <Input
+          name="rua"
+          value={company.rua}
+          placeholder="Rua/Avenida..."
+          onChange={e => setCompany({ ...company, rua: e.target.value })}
+        />
+        <Input
+          name="bairro"
+          value={company.bairro}
+          placeholder="Bairro"
+          onChange={e => setCompany({ ...company, bairro: e.target.value })}
+        />
+        <Input
+          name="cep"
+          value={company.cep}
+          placeholder="CEP"
+          onChange={e => setCompany({ ...company, cep: e.target.value })}
+        />
+        <Input
+          name="complemento"
+          value={company.complemento}
+          placeholder="Complemento"
+          onChange={e =>
+            setCompany({ ...company, complemento: e.target.value })
+          }
+        />
+        <Input
+          name="cidade"
+          value={company.cidade}
+          placeholder="Cidade"
+          onChange={e => setCompany({ ...company, cidade: e.target.value })}
+        />
+        <Input
+          name="estado"
+          value={company.estado}
+          placeholder="Estado"
+          onChange={e => setCompany({ ...company, estado: e.target.value })}
+        />
+        <button type="submit" onClick={() => handleSubmit}>
+          Cadastrar
+        </button>
         <Link to="/list">Empresas cadastradas</Link>
       </Form>
     </Container>
